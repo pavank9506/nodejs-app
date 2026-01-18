e257e2dcb6e3a074c1eb177b6d80bda1cef08169
Your Name <pavan.kota439@email.com>
2026-01-18 13:07:41 +0530

    commiting messages

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   testMatch: ["**/tests/**/*.test.js"],
   collectCoverage: true,
   coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
